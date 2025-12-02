7e03915c90faba0c4bec943952f38f17a8775261
Rcloud173 <amirshaikh17543@gmail.com>
2025-12-02 21:19:52 +0530

    GUI_using streamlit

--- DataOps Console/app.py
+++ DataOps Console/app.py
@@ -1,12 +1,21 @@
-import pandas as pd
-import numpy as np
-import mysql.connector
+from logging import exception
 
-def connect_to_db():
-    
-    return mysql.connector.connect(
-    host = "localhost"
-    user = "root"
-    password = "amiramir123#"
-    database = "data_ops"
+import streamlit as st
+import pandas as pd
+from db_functions import(
+connect_to_db,
+get_basic_info,
+get_additonal_tables,
+get_categories,
+get_suppliers,
+add_new_manual_id, get_all_products, get_product_history, place_reorder,
+get_pending_reorders,mark_reorder_as_received
 )
+
+# sidebar
+st.sidebar.title(" Inventory Management Dashboard")
+option = st.sidebar.radio("Select Option:", ["Basic Information","Operational Tasks"])
+
+
+
+
