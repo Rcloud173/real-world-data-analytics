ebcf62401d0a78ab2d1ca61d669fb29181438b58
Rcloud173 <amirshaikh17543@gmail.com>
2025-12-02 21:20:54 +0530

    reorder

--- DataOps Console/app.py
+++ DataOps Console/app.py
@@ -17,5 +17,101 @@ st.sidebar.title(" Inventory Management Dashboard")
 option = st.sidebar.radio("Select Option:", ["Basic Information","Operational Tasks"])
 
 
+# main space
+st.title("Inventory and Supply Chain Dashboard")
+db=connect_to_db()
+cursor=db.cursor(dictionary=True)
+
+ # --------------------- BASIC INFORMATION PAGE ---------------4
+if option=="Basic Information":
+    st.header("Basic Metrics")
+
+    # get basic information from DB
+    basic_info = get_basic_info(cursor)
+
+    cols=st.columns(3)
+    keys=list(basic_info.keys())
+
+    for i in range(3):
+        cols[i].metric(label=keys[i], value= basic_info[keys[i]])
 
 
+    cols= st.columns(3)
+    for i in range(3,6):
+        cols[i-3].metric(label=keys[i], value= basic_info[keys[i]])
+
+    st.divider()
+
+    # Fetch and display detailed tables
+    tables=get_additonal_tables(cursor)
+    for labels, data in tables.items():
+       st.header(labels)
+       df= pd.DataFrame(data)
+       st.dataframe(df)
+       st.divider()
+
+elif option == "Operational Tasks":
+    st.header("Operational Tasks")
+    selected_task= st.selectbox("Choose an Task",["Add New Product","Product History","Place Reorder","Receive Reorder"])
+    if selected_task =="Add New Product":
+        st.header("Add New Product")
+        categories=get_categories(cursor)
+        suppliers= get_suppliers(cursor)
+
+        with st.form("Add_Product_Form"):
+            product_name=st.text_input("Product_Name")
+            product_category=st.selectbox("Category",categories)
+            product_price=st.number_input("Price", min_value=0.00)
+            product_stock= st.number_input("Stock Quantity", min_value=0, step=1)
+            product_level = st.number_input("Reorder level", min_value=0, step=1)
+
+            supplier_ids=[s["supplier_id"] for s in suppliers]
+            supplier_names=[s["supplier_name"] for s in suppliers]
+
+            supplier_id= st.selectbox(
+                "Supplier",
+                options=supplier_ids,
+                format_func=lambda x: supplier_names[supplier_ids.index(x)]
+            )
+
+            submitted=st.form_submit_button("Add Product")
+
+            if submitted:
+                if not product_name:
+                    st.error("Please Enter the Product Name.")
+                else:
+                    try:
+                        add_new_manual_id(cursor,
+                                          db,
+                                          product_name,
+                                          product_category,
+                                          product_price,
+                                          product_stock,
+                                          product_level,
+                                          supplier_id,
+                                          )
+                        st.success(f"Product {product_name} added successfully")
+                    except Exception as e:
+                             st.error("f Error adding the Product {e}")
+
+# ----------------------------- product History ---------------------
+
+    if selected_task== "Product History":
+        st.header("Product Inventory History")
+
+        #Get product List
+        products=get_all_products(cursor)
+        product_names=[p['product_name'] for p in products]
+        product_ids = [p['product_id'] for p in products]
+
+        selected_product_name=st.selectbox("Select An product", options=product_names)
+
+        if selected_product_name:
+            selected_product_id= product_ids[product_names.index(selected_product_name)]
+            history_data =get_product_history(cursor, selected_product_id)
+
+            if history_data:
+                df= pd.DataFrame(history_data)
+                st.dataframe(df)
+            else:
+                st.info("No History found for the product Selected")
